e9cc97c5b204d8ee9257bcea0f4fa2dc0ef26a76
SunLighttt <974951303@qq.com>
2019-03-11 14:01:14 +0800

    Initial commit

---
+++ src/main/java/com/zut/partymanager/PartymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.zut.partymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartymanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zut/partymanager/PartymanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zut.partymanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PartymanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
